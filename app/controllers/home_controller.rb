ab5e2cea89cf4e58fd66a947976785ddcaaff3b7
AlexMasonAM <alexmmason@gmail.com>
2015-01-14 14:48:12 -0800

    add custom params for Name and Language

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
-    render plain: "Hello"
+  end
+  
+  def next
+    name = params[:name]
+    favorite = params[:favorite]
+    string = "My name is #{name}. My favorite language is #{favorite}."
+    render plain: string
   end
 end
+
